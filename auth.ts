4f1c049b6ee8762dfd8f3defa8dd607f9fce9a11
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-01-23 17:19:39 -0500

    Added callbacks

--- auth.ts
+++ auth.ts
@@ -1,8 +1,15 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
+import { getUserById } from "@/data/user";
 import { db } from "./lib/db";
 import authConfig from "./auth.config";
+
+declare module "next-auth" {
+  interface User {
+    role: "ADMIN" | "USER";
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -10,6 +17,38 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    // async signIn({ user }) {
+    //   const existingUser = await getUserById(user.id);
+
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+
+    //   return true;
+    // },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
