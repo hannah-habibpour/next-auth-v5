0f1e88d7c43bcd6b3fd9ab7b15d6d747db2290ad
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-01-26 17:03:06 -0500

    Added User Button

---
+++ app/(protected)/_components/navbar.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@/components/auth/user-button";
+
+export const Navbar = () => {
+  const pathname = usePathname();
+  return (
+    <nav className="bg-secondary flex justify-between items-center p-4 rounded-xl w-[600px] shadow-sm">
+      <div className="flex gap-x-2">
+        <Button asChild variant={pathname === "/admin" ? "default" : "outline"}>
+          <Link href="/admin">admin</Link>
+        </Button>
+        <Button
+          asChild
+          variant={pathname === "/client" ? "default" : "outline"}
+        >
+          <Link href="/client">client</Link>
+        </Button>
+        <Button
+          asChild
+          variant={pathname === "/server" ? "default" : "outline"}
+        >
+          <Link href="/server">Server</Link>
+        </Button>
+        <Button
+          asChild
+          variant={pathname === "/settings" ? "default" : "outline"}
+        >
+          <Link href="/settings">Settings</Link>
+        </Button>
+      </div>
+      <UserButton />
+    </nav>
+  );
+};
--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,18 +1,21 @@
-import { auth, signOut } from "@/auth";
+"use client";
 
-export default async function SettingsPage() {
-  const session = await auth();
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { signOut } from "next-auth/react";
+
+export default function SettingsPage() {
+  const user = useCurrentUser();
+
+  const onClick = () => {
+    signOut();
+  };
+
   return (
-    <div>
-      {JSON.stringify(session)}
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-      >
-        <button type="submit">Sign out</button>
-      </form>
+    <div className="bg-white p-10 rounded-xl">
+      {/* {JSON.stringify(user)} */}
+      <button onClick={onClick} type="submit">
+        Sign out
+      </button>
     </div>
   );
 }
